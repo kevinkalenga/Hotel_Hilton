97a9860868eda46667aa492afbeb4dc26704508a
Kalenga243 <kevinkalenga10@gmail.com>
2025-09-23 01:11:37 +0200

    Creation migration and models for admins table

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin;
+use Hash;
+use Auth;
 
 class AdminLoginController extends Controller
 {

8a0e117343a7bbe739acea954dd2b54e9b96b0c5
Kalenga243 <kevinkalenga10@gmail.com>
2025-09-23 12:43:57 +0200

    Login, logout, error message and admin guard setup

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -23,4 +23,30 @@ class AdminLoginController extends Controller
     {
         return view('admin.forget_password');
     }
+    public function login_submit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        
+        // if everything is okey, i get my fields
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        // i use my auth guard which is admin
+        if(Auth::guard('admin')->attempt($credential)) {
+            return redirect()->route('admin_home');
+        } else {
+            return redirect()->route('admin_login')->with('error', 'Information is not correct!');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login');
+    }
 }
